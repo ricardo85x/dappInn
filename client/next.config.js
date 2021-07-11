d75fc3b6e9b6ddecb1087915371f60c82f31ed69
Ricardo <ricardoinliner@yahoo.com>
2021-07-11 14:07:13 -0300

    setup vercel

--- client/next.config.js
+++ client/next.config.js
@@ -2,19 +2,19 @@ const withImages = require('next-images')
 const withSass = require('sass')
 
 const withPlugins = require('next-compose-plugins');
-
 const isDev = process.env.NODE_ENV !== "production";
 
 
 module.exports = withPlugins([
-    [withImages, {
-      assetPrefix: isDev ? "": "/dappInn/",
-      basePath: isDev ? "": "/dappInn"
-    }],
-    withSass,
-    {
-        trailingSlash: true,
-        basePath: isDev ? "": "/dappInn",
-        assetPrefix: isDev ? "": "/dappInn/"    
-    }
+  withImages
+    // [withImages, {
+    //   assetPrefix: isDev ? "": "/dappInn/",
+    //   basePath: isDev ? "": "/dappInn"
+    // }],
+    // withSass,
+    // {
+    //     trailingSlash: true,
+    //     basePath: isDev ? "": "/dappInn",
+    //     assetPrefix: isDev ? "": "/dappInn/"    
+    // }
 ])
